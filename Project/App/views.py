c74fceef827aaeb1f01fc00166a8d23af6822d94
Pablo Gareis <gareispablotecnica1@gmail.com>
2026-03-19 17:12:34 -0300

    Actualzado

--- Project/App/views.py
+++ Project/App/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
-
+from .models import *
 # Create your views here.
 def Home(request):
     return render(request,'Base.html')
 
 def Productos(request):
-    return render(request,'Productos.html')
+    # ---> Guardamos todos los Productos en una variable
+    Villalva=Producto.objects.all().order_by[:3]
+    data={
+        # ---> Enviamos todos los productos a la palabra reservada
+        'tabla':Villalva
+    }
+    # --------------->Enviamos mediante la Palabra Data
+    return render(request,'Productos.html',data)
